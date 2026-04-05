72ca995dad029a072a524cd0eb16b26de97f8854
Rajesh <rajesh@staplelogic.in>
2026-04-05 09:41:54 +0530

    Add image on email

--- resources/views/emails/send_query_form.blade.php
+++ resources/views/emails/send_query_form.blade.php
@@ -4,16 +4,9 @@
 <table class="wrapper" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #f5f8fa; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%" width="100%" cellspacing="0" cellpadding="0"><tbody><tr><td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box" align="center">
                 <table class="content" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%" width="100%" cellspacing="0" cellpadding="0"><tbody><tr><td class="header" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 25px 0; text-align: center">
 
-
-                    <a href="{{url('/')}}" style="color:inherit;" target="_blank"> <img src="https://vaikunthholidays.in/public/img/logo/logo.png" alt="Vaikunth Holidays Pvt Limited" width="145" align="center" style="-ms-interpolation-mode:bicubic;border:none;outline:none;border-style:none;display: none;"> 
-                      <h3 style="font-family:Helvetica,Arial,sans;font-weight:bold;font-size:32px;color:rgb(34,31,31);line-height:36px;">Amendi.ly</h3>
-                      </a>
+        <a href="{{url('/')}}" style="color:inherit;" target="_blank" > <img src="{{ asset('public/img/logo/logo.png') }}" alt="Logo" width="400" align="center" height="145"></a>
 
 
-
-        <!-- <a href="{{url('/')}}" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #bbbfc3; font-size: 19px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 white" target="_blank" rel="noreferrer">
-            Vaikunth Holidays Pvt Limited
-        </a> -->
     </td>
 </tr><tr><td class="body" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #FFFFFF; border-bottom: 1px solid #EDEFF2; border-top: 1px solid #EDEFF2; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%" width="100%">
                             <table class="inner-body" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #FFFFFF; margin: 0 auto; padding: 0; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px" width="570" cellspacing="0" cellpadding="0" align="center"><tbody><tr><td class="content-cell" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px">
